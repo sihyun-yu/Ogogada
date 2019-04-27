342a1e11ec66835eee16ab08e450671a09bdbb40
sihyun-yu <yusihyunc@kaist.ac.kr>
2019-04-27 22:22:28 +0900

    changed init start file

--- Project/ogogada/src/actions/menuActions.js
+++
@@ -1,7 +0,0 @@
-import * as types from './actionTypes';
-
-const loadMenu = () => ( {
-  type: types.LOADING_MENU,
-} );
-
-export default loadMenu;
--- Project/ogogada/src/containers/app/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './index';
-
-describe( 'containers.app', () => {
-  it( 'renders without crashing', () => {
-    shallow( <App /> );
-  } );
-} );
