1e430b6ad13c9789cae2f740f0af2085155e86e6
sihyun-yu <yusihyunc@kaist.ac.kr>
2019-04-27 20:22:43 +0900

    added init files for front-end

--- Project/ogogada/src/index.js
+++ Project/ogogada/src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import App from "./components/app/App";
+import store from "./store";
+
+import registerServiceWorker from "./registerServiceWorker";
+
+import "./index.css";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route component={App} />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
+registerServiceWorker();
