e467afac19bd61da050234827645ab9fc0bed70c
나 <doubleb@kaist.ac.kr>
2019-05-10 16:33:56 +0900

    from level to history added

--- ogogada-react/src/components/Home/HomeComponent.jsx
+++ ogogada-react/src/components/Home/HomeComponent.jsx
@@ -3,36 +3,58 @@ import windowSize from 'react-window-size';
 import { Subscribe } from "unstated";
 import {HomeStore} from "../../stores";
 import { Input, Button, Image } from 'semantic-ui-react';
+import { withRouter} from 'react-router-dom';
+import ImageMapper from 'react-image-mapper';
 import "../../stylesheets/Home.css"
-import Dimensions from 'react-dimensions';
 
-
-function defaultGetDimensions (element) {
-    return [element.clientWidth, element.clientHeight]
-  }
-
-
-var bg = require("../../assets/stage.png")
 class HomeComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
 
         };
+        //this.routeChange = this.routeChange.bind(this);
         
     }
-
+    
+    routeChange() {
+        this.props.history.push({
+            pathname : '../history',
+            state: {
+            }
+        });
+    }
 
 
     render() {
 
-        console.log("home", this.props.location.state.id);
+        console.log("home", this.props);
         const id = this.props.location.state.id;
         const pw = this.props.location.state.pw;
         const level = this.props.location.state.level;
 
         var x1 = this.props.windowWidth * 211/1280;
         var y1 = this.props.windowHeight * 114/720;
+        var x2 = this.props.windowWidth * 425/1280;
+        var y2 = this.props.windowHeight * 228/720;
+        var x3 = this.props.windowWidth * 641/1280;
+        var y3 = this.props.windowHeight * 352/720;
+        var x4 = this.props.windowWidth * 852/1280;
+        var y4 = this.props.windowHeight * 471/720;
+        var x5 = this.props.windowWidth * 1066/1280;
+        var y5 = this.props.windowHeight * 593/720;
+
+        var URL = require("../../assets/stage.png")
+        var MAP = {
+        name: "my-map",
+        areas: [
+            { name: "1", shape: "circle", coords: [x1, y1, 63], },
+            { name: "2", shape: "circle", coords: [x2, y2, 63],},
+            { name: "3", shape: "circle", coords: [x3, y3, 63], },
+            { name: "4", shape: "circle", coords: [x4, y4, 63], },
+            { name: "5", shape: "circle", coords: [x5, y5, 63] },
+        ]
+        }
 
         console.log(id);
         console.log(pw);
@@ -45,14 +67,11 @@ class HomeComponent extends React.Component {
                     <div
                         className = "stage"
                     > 
-                    {(x1 || y1) &&
-                        <map name="image-map">
-                            <area id="areas" shape="circle" coords={"136" + "," + "1083" + ",63"}
-                                href="../history"
-                            />
-                        </map>
+                        <ImageMapper src={URL} map={MAP} width={this.props.windowWidth} height={this.props.windowHeight}
+                            onClick={area => this.routeChange()}
+                        
+                        />
 
-                    }
                     </div>
                     )
                     
@@ -62,26 +81,6 @@ class HomeComponent extends React.Component {
     }
 }
 
-/*
-const HomeComponent = props => {
-    console.log("home", props.location.state.id);
-    const id = props.location.state.id;
-    const pw = props.location.state.pw;
-    const level = props.location.state.level;
-    console.log(id);
-    console.log(pw);
-    console.log(level);
-    return (
-        <Subscribe to = {[HomeStore]}>
-            {user => (
-                <div>          
-                    {user.update(id)}
-                </div>
-                
-            )}
-        </Subscribe>
-    );
-};
-*/
 
-export default windowSize(HomeComponent);
+
+export default withRouter(windowSize(HomeComponent));
