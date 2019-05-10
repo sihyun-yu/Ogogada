3722cdcb54a1952b1f09ec0df8bbfba7928ce7ac
나 <doubleb@kaist.ac.kr>
2019-05-10 15:13:49 +0900

    level page size updated

--- ogogada-react/src/components/Home/HomeComponent.jsx
+++ ogogada-react/src/components/Home/HomeComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import windowSize from 'react-window-size';
 import { Subscribe } from "unstated";
 import {HomeStore} from "../../stores";
 import { Input, Button, Image } from 'semantic-ui-react';
@@ -21,48 +22,22 @@ class HomeComponent extends React.Component {
         
     }
 
-    getWrappedInstance () {
-        return this.refs.wrappedInstance
-    }
-
-
-    updateDimensionsImmediate = () => {
-        const dimensions = defaultGetDimensions(this._parent)
-
-        if (dimensions[0] !== this.state.containerWidth ||
-            dimensions[1] !== this.state.containerHeight) {
-          this.setState({
-            containerWidth: dimensions[0],
-            containerHeight: dimensions[1]
-          })
-        }
-    }
-
-    componentDidMount(){
-        console.log(this.refs)
-        if (!this.refs.wrapper) {
-            throw new Error('Cannot find wrapper div')
-          }
-          this._parent = this.refs.wrapper.parentNode
-          this.updateDimensionsImmediate()
-    }
-
 
 
     render() {
-        const {containerWidth, containerHeight} = this.state
-        if (this._parent && !containerWidth && !containerHeight) {
-          // only trigger a warning about the wrapper div if we already have a reference to it
-          console.warn('Wrapper div has no height or width, try overriding style with `containerStyle` option')
-        }
+
         console.log("home", this.props.location.state.id);
         const id = this.props.location.state.id;
         const pw = this.props.location.state.pw;
         const level = this.props.location.state.level;
 
+        var x1 = this.props.windowWidth * 0.5;
+        var y1 = this.props.windowHeight * 0.5;
+
         console.log(id);
         console.log(pw);
         console.log(level);
+        console.log("x1, y1: ", x1, y1);
 
         return (
             <Subscribe to = {[HomeStore]}>
@@ -70,9 +45,9 @@ class HomeComponent extends React.Component {
                     <div
                         className = "stage"
                     > 
-                    {(containerWidth || containerHeight) &&
+                    {(x1 || y1) &&
                         <map name="image-map">
-                        <area id="areas" shape="circle" coords={containerWidth.toString() + "," + containerHeight.toString() + ",5"}/>
+                        <area id="areas" shape="circle" coords={x1.toString() + "," + y1.toString() + ",5"}/>
                         </map>
 
                     }
@@ -107,4 +82,4 @@ const HomeComponent = props => {
 };
 */
 
-export default HomeComponent;
+export default windowSize(HomeComponent);
