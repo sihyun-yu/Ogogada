0d93a3ecc982daaf897051659fc1f141c83a07a5
나 <doubleb@kaist.ac.kr>
2019-05-13 04:41:13 +0900

    pop up question added

--- ogogada-react/src/components/Pos/Description/DescriptionComponent.jsx
+++ ogogada-react/src/components/Pos/Description/DescriptionComponent.jsx
@@ -2,28 +2,40 @@ import React from "react";
 import "../../../stylesheets/Description.css";
 import { Button, Icon } from 'semantic-ui-react'
 import Timer from "./Timer.jsx"
+import MenuList from "../MenuList/MenuListComponent";
+import QuestionDialog from "../MenuList/QuestionDialogComponent";
+import { MenuStore } from "../../../stores";
+import { Subscribe } from "unstated";
 //import Button from "@material-ui/core/Button";
 
 const DescriptionComponent = props => {
   return (
-    <div className="description__container">
-      <div className="description" >
-        <p>Level: {props.level}</p>
-      </div>
-      <div className="user_id">
-        <p>ID: {props.username}</p>
-      </div>
-      <div className="timer">
-        <div className="timer__icon">
-          <Icon name='stopwatch'></Icon>
+    <div>
+    <Subscribe to={[MenuStore]}>
+      {(menu) => (
+        <div>
+          <div className="description__container">
+            <div className="description" >
+              <p>Level: {props.level}</p>
+            </div>
+            <div className="user_id">
+              <p>ID: {props.username}</p>
+            </div>
+            <div className="timer">
+              <div className="timer__icon">
+                <Icon name='stopwatch'></Icon>
+              </div>
+              <div className="timer__timer">
+                <Timer></Timer>
+              </div>
+            </div>
+            <div className="replay" onClick={menu.openQuestion.bind(menu)}>
+              <Icon name='tasks' size='small'>task</Icon>
+            </div>
+          </div>
         </div>
-        <div className="timer__timer">
-          <Timer></Timer>
-        </div>
-      </div>
-      <div className="replay">
-        <Icon name='sound'></Icon>
-      </div>
+      )}
+    </Subscribe>
     </div>
     
   );
