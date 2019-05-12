a60e408ea5f9d471bb0096583f8cbfc2134e651b
Kim Yoonseo <kimyoonseo@kaist.ac.kr>
2019-05-12 19:05:26 +0900

    menu and price fixed

--- ogogada-react/src/assets/meta.js
+++ ogogada-react/src/assets/meta.js
@@ -5,25 +5,25 @@ const meta = {
     ["Americano ICE", 1800, 1, 0],
     ["Caffe Latte HOT", 2000, 2, 0],
     ["Caffe Latte ICE", 2300, 2, 0],
-    ["Caramel macchiato HOT", 2500, 2, 0],
-    ["Caramel macchiato ICE", 2800, 2, 0],
+    ["Caramel Macchiato HOT", 2500, 2, 0],
+    ["Caramel Macchiato ICE", 2800, 2, 0],
     ["Caffe Mocha HOT", 2500, 2, 0],
     ["Caffe Mocha ICE", 2800, 2, 0]
   ],
   menus_right: [
-    ["a", 1000, 3, 1],
-    ["b", 1800, 3, 1],
-    ["c", 2000, 3, 1],
-    ["d", 2300, 3, 1],
-    ["e", 2500, 4, 1],
-    ["f", 2800, 4, 1],
-    ["g", 2500, 4, 1],
-    ["h", 2800, 4, 1],
+    ["Green Tea Latte HOT", 2500, 3, 1],
+    ["Green Tea Latte ICE", 2800, 3, 1],
+    ["Earl Grey HOT", 2000, 3, 1],
+    ["Earl Grey ICE", 2300, 3, 1],
+    ["Chamomile HOT", 2000, 4, 1],
+    ["Chamomile ICE", 2300, 4, 1],
+    ["Peppermint HOT", 2000, 4, 1],
+    ["Peppermint ICE", 2300, 4, 1],
   ],
   coupons: [0, 0.05, 0.1],
   paymentMethods: [
-    { type: "card", label: "카드", discount: 0 },
-    { type: "cash", label: "현금", discount: 0.05 }
+    { type: "card", label: "card", discount: 0 },
+    { type: "cash", label: "cash", discount: 0.05 }
   ],
   numbers: [
   "7", "8", "9",
