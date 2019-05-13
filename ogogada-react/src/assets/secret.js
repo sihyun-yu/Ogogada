ab23125155d52aca796c0557761a70d7e51df843
나 <doubleb@kaist.ac.kr>
2019-05-13 15:21:11 +0900

    fadf

---
+++ ogogada-react/src/assets/secret.js
@@ -0,0 +1,12 @@
+/*Firebase 이용 DB 여기서 받아오기*/
+const firebaseConfig = {
+    apiKey: "AIzaSyDXOPz3fKpgr5p4b99tNj4QnKVzqeXsxyU",
+    authDomain: "ogogada-dp6.firebaseapp.com",
+    databaseURL: "https://ogogada-dp6.firebaseio.com",
+    projectId: "ogogada-dp6",
+    storageBucket: "ogogada-dp6.appspot.com",
+    messagingSenderId: "327472332391",
+    appId: "1:327472332391:web:9f65dd33579f2761"
+  };
+
+  export default firebaseConfig;
